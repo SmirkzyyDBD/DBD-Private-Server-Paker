d18bae7d54fb1a7ddc9a708624e51c330323048c
Smirkzyy <141465520+SmirkzyyDBD@users.noreply.github.com>
2024-08-03 03:54:50 +0100

    Add files via upload
    Changed `CreateMod - PrivServer.bat` to allow the use of multiple folders.

--- PrivServerPaker/CreateMod - PrivServer.bat
+++ PrivServerPaker/CreateMod - PrivServer.bat
@@ -17,14 +17,17 @@ if "%~1"=="" (
 cls
 pushd %~dp0
 set /p "ModNumber=What number would you like after pakchunk? (Higher than 348): "
+set /a Index=0
 for %%a in (%*) do (
+    set /a CurrentNumber=ModNumber+Index
     echo "%%~a\*.*" "..\..\..\*.*" > filelist.txt
     echo Paking mod, this may take a while . . .
-    Resources\UnrealPak.exe "..\Mods/%%~na/pakchunk!ModNumber!%%~na-WindowsNoEditor.pak" -create=../filelist.txt -compress -asynccompression -compressionformat=Oodle -compressmethod=Leviathan -compresslevel=6 -compressionblocksize=256KB -multiprocess
+    Resources\UnrealPak.exe "..\Mods/%%~na/pakchunk!CurrentNumber!%%~na-WindowsNoEditor.pak" -create=../filelist.txt -compress -asynccompression -compressionformat=Oodle -compressmethod=Leviathan -compresslevel=6 -compressionblocksize=256KB -multiprocess
     cd /d "%~dp0\Resources"
-    copy /y "copy.sig" "..\Mods\%%~na\pakchunk!ModNumber!%%~na-WindowsNoEditor.sig"
+    copy /y "copy.sig" "..\Mods\%%~na\pakchunk!CurrentNumber!%%~na-WindowsNoEditor.sig"
     cd /d %~dp0
     del "filelist.txt"
+    set /a Index+=1
 )
 goto :end
 
